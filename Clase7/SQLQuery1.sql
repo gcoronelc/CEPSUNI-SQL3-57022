fd2348adb8f9aa18779a7359d234433d0ac270e0
Gustavo Coronel <gcoronelc@gmail.com>
2021-07-07 21:58:12 -0500

    Clase 7

---
+++ Clase7/SQLQuery1.sql
@@ -0,0 +1,21 @@
+
+select * from dbo.CLIENTES;
+go
+
+select * from dbo.EMPLEADO;
+go
+
+select * from dbo.PEDIDOS;
+go
+
+
+select * from dbo.tbl_curso;
+go
+
+drop table dbo.tbl_profesor;
+go
+
+select * from dbo.tblprofesor;
+go
+
+
